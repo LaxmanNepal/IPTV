c6cd0317c6ebc3f90693361febcbeb70b1667014
Laxman Nepal <84069546+LaxmanNepal@users.noreply.github.com>
2026-09-12 11:44:32 +0300

    Add lightweight IPTV service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='laxman-iptv-v1';
+const APP=['./','./index.html','./style.css','./app-tv.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin===location.origin&&!u.pathname.endsWith('.m3u'))e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
